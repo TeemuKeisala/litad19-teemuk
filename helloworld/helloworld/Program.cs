6ff77e7a693b452afce30fe0821745b814492189
TeemuKeisala <teemukeisala1@gmail.com>
2019-09-04 10:58:51 +0300

    print helloworld

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Hello
+    {
+        static void Main();
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
