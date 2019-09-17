56b57db7627e924c845e4c2d55c1e3dce42842eb
TeemuKeisala <teemukeisala1@gmail.com>
2019-09-17 11:11:18 +0300

    koodia.

---
+++ exampleAverage/exampleAverage/exampleAverage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exampleAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
