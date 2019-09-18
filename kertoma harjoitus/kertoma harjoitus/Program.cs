04c4975abaf4aad82af6520cbf2ad519a9cee6ff
TeemuKeisala <teemukeisala1@gmail.com>
2019-09-18 15:47:10 +0300

    valmis

--- kertoma harjoitus/kertoma harjoitus/Program.cs
+++ kertoma harjoitus/kertoma harjoitus/Program.cs
@@ -6,7 +6,37 @@ namespace kertoma_harjoitus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kertoman laskenta");
+            //Define variables.
+            int n= 0;
+            int fact = 1;
+
+            //Tulostetaan käyttäjälle.
+            Console.WriteLine("Kertoman laskenta.");
+            Console.Write("Minkä luvun kertoma lasketaan? ");
+
+            //Luetaan käyttäjän syöte ja pakotetaan se interger muotoon.
+            n =int.Parse(Console.ReadLine());
+
+            //Ehto, jossa testataan käyttäjän syöttämä arvo.
+            //Ohjelman logiikka.
+            if (n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte.");
+            }
+
+            else
+
+
+            {
+                //Silmukka, jossa lasketaan kertoman arvo.
+                while (n > 0)
+                {
+                    fact = fact * n; //Lasketaan kertoma joka kierroksella.
+                    n = n - 1; //Vähennetään arvoa yhdellä.
+                }
+                //Tulosta vastaus näytölle.
+                Console.WriteLine("vastaus:" + fact);
+            }
         }
     }
 }
