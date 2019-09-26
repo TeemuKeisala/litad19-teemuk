a571445b9bcd88e8c74f6ac5fed5886626d294b2
TeemuKeisala <teemukeisala1@gmail.com>
2019-09-26 13:27:22 +0300

    ReadWriteExample ready code

--- ReadWriteExample/ReadWriteExample/Program.cs
+++ ReadWriteExample/ReadWriteExample/Program.cs
@@ -6,7 +6,16 @@ namespace ReadWriteExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ReadWriteExample");
+            Console.WriteLine("ReadWriteExample"); //Tulostaa näytölle viestin""
+            Console.Write("Moi, kirjoita nimesi: ");//Tulostaa näytölle viestin"" mutta jättää kursorin samalle riville kuin tekstin
+            string userInput = Console.ReadLine(); // Lukee käyttäjän kirjoittaman viestin.
+            Console.WriteLine("-----");
+            Console.WriteLine("Moi" + userInput + "!"); //Vaihtoehto 1
+            Console.WriteLine("Moi {0}!", userInput); //Vaihtoehto 2
+            Console.WriteLine($"Moi {userInput}!"); //Vaihtoehto 3
+            //Tulostuksen muotoilu ja muuttujien käyttö 
+            //tulostuksen yhdteydessä $-merkin avulla = string.Format()
+
         }
     }
 }
