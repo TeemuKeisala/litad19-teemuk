b34b60e4f96729d924a1e5ba5f4b29c46b89bc31
TeemuKeisala <teemukeisala1@gmail.com>
2019-10-01 08:57:48 +0300

    koooodia

---
+++ debug.code/debug.code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace debug.code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää käyttäjän syöttämästä luvusta, onko se positiivinen vai negatiivinen");
+            Console.Write("syötä kokonaisluku:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == 0)
+            {
+                Console.WriteLine($"Luku {userNumber}on positiivinen");
+            }
+            else
+            {
+                if (userNumber > 0)
+                {
+                    Console.WriteLine($"Luku {userNumber}on negatiivinen");
+                }
+            }
+            }
+        }
+    }
+
---
+++ if_task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace if_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelmaselvittää onko luku pariton vai parillinen!");
+        }
+    }
+}
