7c8e08a91021e227838fa8b684ff657feb1db667
TeemuKeisala <teemukeisala1@gmail.com>
2019-10-14 14:13:15 +0300

    if-HARJOITUS 2 KESKEN

---
+++ If_harjoitus 2/If_harjoitus 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace If_harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 16;
+           
+            Console.WriteLine("Syötä ikäsi.");
+            int x = int.Parse(Console.ReadLine());
+            if (x <= 7)
+                Console.WriteLine("Ilmainen");
+            if (x <= 15)
+                Console.WriteLine($"Sinun hintasi on {y*0.5} €");
+            if (x >= 65)
+                Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
+            else if (x <= 65)
+                Console.WriteLine($"Sinun hintasi on {y} €");
+
+            Console.WriteLine("Oletko varusmies?");
+            if userInput = ("kyllä");
+                Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
+            if userInput = ("ei")
+            Console.WriteLine("Oletko opiskelija?");
+            Console.WriteLine("Oletko MTK jäsen?");
+
+
+        }
+    }
+}
