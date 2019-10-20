d2b20c4ddb7cc0d814b15aefa9210aad55c45e47
TeemuKeisala <teemukeisala1@gmail.com>
2019-10-20 23:23:03 +0300

    IF_harjoitus 2

--- If_harjoitus 2/If_harjoitus 2/Program.cs
+++ If_harjoitus 2/If_harjoitus 2/Program.cs
@@ -7,26 +7,67 @@ namespace If_harjoitus_2
         static void Main(string[] args)
         {
             int y = 16;
-           
+
             Console.WriteLine("Syötä ikäsi.");
             int x = int.Parse(Console.ReadLine());
             if (x <= 7)
                 Console.WriteLine("Ilmainen");
-            if (x <= 15)
-                Console.WriteLine($"Sinun hintasi on {y*0.5} €");
-            if (x >= 65)
+            else if (x <= 15)
+                Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
+            else if (x >= 65)
                 Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
             else if (x <= 65)
-                Console.WriteLine($"Sinun hintasi on {y} €");
-
-            Console.WriteLine("Oletko varusmies?");
-            if userInput = ("kyllä");
-                Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
-            if userInput = ("ei")
-            Console.WriteLine("Oletko opiskelija?");
-            Console.WriteLine("Oletko MTK jäsen?");
+            {
+                //Console.WriteLine($"Sinun hintasi on {y} €");
 
 
+                Console.Write("Oletko varusmies? [K/E]: ");
+                string isConscipt = Console.ReadLine().ToUpper();
+                if (isConscipt == "K")
+                {
+                    Console.WriteLine($"Sinun hintasi on {y * 0.5} €");
+                }
+                else
+                {
+                    Console.Write("Oletko opiskelija? [K/E]: ");
+                    string isStudent = Console.ReadLine().ToUpper();
+                    if (isStudent == "K")
+                    {
+                        Console.Write("Oletko MTK jäsen? [K/E]: ");
+                        string isMtkMember = Console.ReadLine().ToUpper();
+                        if (isMtkMember == "K")
+                        {
+                            Console.WriteLine($"Sinun hintasi on {y * 0.45} €");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sinun hintasi on {y * 0.8} €");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Oletko MTK jäsen? [K/E]: ");
+                        string isMtkMember = Console.ReadLine().ToUpper();
+                        if (isMtkMember == "K")
+                        {
+                            Console.WriteLine($"Sinun hintasi on {y * 0.85} €");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sinun hintasi on {y} €");
+                        }
+
+                    }
+
+
+
+
+
+                }
+
+
+
+            }
         }
     }
 }
